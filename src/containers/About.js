c4861ba4a74a5f60de0e128c89379fbd4de32732
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-04-18 10:30:28 +0800

    Add introduction to About

--- src/containers/About.js
+++ src/containers/About.js
@@ -22,6 +22,7 @@ const About = () => (
     <Row className="container About">
       <Col span={8}>
         <Anchor style={{ paddingTop: 20 }}>
+          <Anchor.Link href="#introduction" title="Introduction" />
           <Anchor.Link href="#what-is-cryptocanvas" title="What is CryptoCanvas?" />
           <Anchor.Link href="#how-does-it-work" title="How does it work?">
             <Anchor.Link href="#painting" title="I. Painting" />
@@ -36,6 +37,33 @@ const About = () => (
         </Anchor>
       </Col>
       <Col span={16}>
+        <h2 id="introduction"><b>Introduction</b></h2>
+
+        <p>An inspiration for creating CryptoCanvas came from the combination of two ideas. First, we got really excited
+          about some amazing projects based on the Ethereum blockchain, such as <a
+            href="https://www.larvalabs.com/cryptopunks" target="_blank" rel="noopener noreferrer">CryptoPunks</a> and <a
+            href="https://cryptokitties.co" target="_blank" rel="noopener noreferrer">CryptoKitties</a>. They showed us
+          there is a completely new approach to developing apps and creating a community. However, we realized they
+          still heavily rely on the centralized servers - the blockchain stores only some ID or DNA numbers which can
+          turn into a Punk or a Kitty only when interpreted by the creators' servers.</p>
+
+        <p>We started to wonder whether it would be possible to store ALL the data on the blockchain, which would result
+          in a truly decentralized, independent app.</p>
+
+        <p>As it turned out, storing any larger amount of data on the blockchain is insanely expensive. And here, the
+          second inspiration struck us - <a href="https://redditblog.com/2017/04/18/place-part-two/" target="_blank"
+                                            rel="noopener noreferrer">Reddit Place</a>, a unique experiment performed by
+          the Reddit community. It is a 1000x1000 pixel artwork created together by thousands of participants. It proved
+          that human creativity and ability to cooperate is endless and given the right opportunity, can result in some
+          incredible outcome.</p>
+
+        <p>By combining these two ideas, we came up with the solution. Storing data on the blockchain, even though
+          expensive for a single person, becomes cheap and accessible when shared by thousands of people. And when these
+          people connect together to create an artwork which will exist forever, it results in a completely new form of
+          art.</p>
+
+        <p>Ladies and gentlemen, we proudly present to you the CryptoCanvas.</p>
+
         <h2 id="what-is-cryptocanvas"><b>What is CryptoCanvas?</b></h2>
 
         <p>CryptoCanvas is a limited set of collectible 64x64 pixel artworks to-be-created by the CryptoCanvas
@@ -49,7 +77,7 @@ const About = () => (
         <p>A canvas can be in one of three states:</p>
 
         <h3 id="painting"><b>I. Painting</b></h3>
-        <p>A canvas is being painted while as long as there is still at least one pixel without a color. You can place a
+        <p>A canvas is being painted as long as there is still at least one pixel without a color. You can place a
           pixel of any color of your choosing anywhere on the canvas, even on the already existing pixel. </p>
 
         <h3 id="initial-bidding"><b>II. Initial Bidding</b></h3>
@@ -59,11 +87,12 @@ const About = () => (
         <p>In the real world, when an artist finishes an artwork, she offers it for sale. CryptoCanvas has the same
           principle - the only difference is that it has many authors instead of one.</p>
 
-        <p>To determine its first owner, the canvas is put up for an auction. Other users willing to make a bid will
-          have only 48 hours to do so. After this time, a bidder with the last, highest bid becomes the sole owner of
-          the canvas. Amount of Ether from the bid is evenly distributed to all the authors of the canvas, based on how
-          many pixels appearing on the completed artwork they placed. The more pixels were placed by you on the final
-          canvas, the bigger will be your reward!</p>
+        <p>To determine its first owner, the canvas is put up for an auction. Anyone, including the authors of the
+          pixels, can make a bid. Once the first bid on the canvas is placed, a clock starts to count down the remaining
+          time for bidding. Other users willing to make a bid will have only 48 hours to do so. After this time, a
+          bidder with the last, highest bid becomes the sole owner of the canvas. Amount of Ether from the bid is evenly
+          distributed to all the authors of the canvas, based on how many pixels appearing on the completed artwork they
+          placed. The more pixels were placed by you on the final canvas, the bigger will be your reward!</p>
 
         <h3 id="trading"><b>III. Trading</b></h3>
         <p>Once a canvas has an owner, it can be traded in the "Marketplace".</p>
