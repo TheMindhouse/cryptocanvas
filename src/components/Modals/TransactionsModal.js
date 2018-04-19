22a7dd0fec15eebffe910aa0f89bafd3f8cb706f
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-04-19 15:19:03 +0800

    Fix imports

--- src/components/Modals/TransactionsModal.js
+++ src/components/Modals/TransactionsModal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button, Modal } from 'antd'
-import TransactionsList from '../Account/TransactionsList'
+import TransactionsList from '../AccountWidget/TransactionsList'
 
 class TransactionsModal extends React.PureComponent {
   render() {
