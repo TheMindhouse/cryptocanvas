b46bffe625c7d8d192218275c2f9ba5883ff8578
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-04-29 11:42:31 +0100

    Closes #12, add info if user has been already rewarded for painting pixels

--- src/components/CanvasSidebar/PainterReward/WithReward.js
+++ src/components/CanvasSidebar/PainterReward/WithReward.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Modal } from 'antd'
+import { Alert, Button, Modal } from 'antd'
 
 const WithReward = (props) => {
   const confirm = () => {
@@ -17,22 +17,28 @@ const WithReward = (props) => {
   return (
     <div>
       <p>
-        You've painted <b>{props.paintedPixels}</b> final pixels of this Canvas <span className="emoji-rocket" /> <span className="emoji-hands" />
+        You've painted <b>{props.paintedPixels}</b> final pixels of this Canvas <span className="emoji-rocket" /> <span
+        className="emoji-hands" />
       </p>
       <p>
         Your reward for painting is <span title={props.rewardInEth + ' ETH'}>
-            <b>{parseFloat(Number(props.rewardInEth).toFixed(5))}</b>
-          </span> ETH.
+            <b>{parseFloat(Number(props.rewardInEth).toFixed(5))} ETH</b>
+          </span>.
       </p>
 
-      <Button
-        type="primary"
-        size="default"
-        onClick={confirm}
-        title={props.isWithdrawn ? 'You have already added the reward to your Account Balance.' : ''}
-        disabled={props.isWithdrawn}>
-        Add to my Account Balance
-      </Button>
+      {
+        props.isWithdrawn
+          ? <Alert message="Reward for painting has been added to your Account Balance 🎉" type="success" showIcon />
+          : <Button
+            type="primary"
+            size="default"
+            onClick={confirm}
+            title={props.isWithdrawn ? 'You have already added the reward to your Account Balance.' : ''}>
+            Add to my Account Balance
+          </Button>
+      }
+
+
     </div>
   )
 }
