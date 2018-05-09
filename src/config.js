390f1aaf9fe8ddd41e319bfa503c94a5d861872f
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-05-09 16:29:51 +0100

    Updated Contract address!

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 export const CONFIG = {
-  // CONTRACT_ADDRESS: '0x4d5cafaa44847c01fe22bbc0cf9771b64782fa29', // Local
-  CONTRACT_ADDRESS: '0x5e68426C7bBCEe3F590B95Dd52066AA2efF6165B', // Rinkeby testnet
-  // WEB3_HTTP_PROVIDER: 'http://localhost:8545',
-  WEB3_HTTP_PROVIDER: 'https://rinkeby.infura.io/ML50g9METlqvSTgwiJTm',
+  CONTRACT_ADDRESS: '0x0c9b5c62e7ddeba23164fefa0ff10676e3b52629', // Main net
+  WEB3_HTTP_PROVIDER: 'https://mainnet.infura.io/ML50g9METlqvSTgwiJTm',
   // Delay to check transactions in ms
   CHECK_TX_DELAY: 3000,
   // Delay to check account address in ms
@@ -13,8 +11,8 @@ export const CONFIG = {
   MINIMUM_BID: 0.08,
   startBlock: 228,
   pixelSize: {
-    preview: 64,
-    canvas: 220,
+    preview: 3,
+    canvas: 10,
   },
-  gridColumns: 3,
+  gridColumns: 64
 }
