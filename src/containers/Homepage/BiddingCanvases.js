2aebad93c9f453265c23702d23813427673bc9a4
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-05-09 12:43:45 +0100

    Remove overflow-hidden class in canvas previews

--- src/containers/Homepage/BiddingCanvases.js
+++ src/containers/Homepage/BiddingCanvases.js
@@ -8,23 +8,21 @@ class BiddingCanvases extends React.PureComponent {
   render () {
     const { onBiddingFinished } = this.props
     return (
-      <div className="overflow-hidden">
-        <Row gutter={100} type="flex">
-          {this.props.canvasIds.map((canvasId, index) =>
-            <Col xs={24} sm={12} md={6} key={index}>
-              <CanvasPreview
-                canvasId={canvasId}
-                extraRender={() =>
-                  <HighestBidWatcher
-                    canvasId={canvasId}
-                    onBiddingFinished={onBiddingFinished}
-                    render={(state) => <BiddingCanvasExtras {...state} />} />
-                }
-              />
-            </Col>
-          )}
-        </Row>
-      </div>
+      <Row gutter={100} type="flex">
+        {this.props.canvasIds.map((canvasId, index) =>
+          <Col xs={24} sm={12} md={6} key={index}>
+            <CanvasPreview
+              canvasId={canvasId}
+              extraRender={() =>
+                <HighestBidWatcher
+                  canvasId={canvasId}
+                  onBiddingFinished={onBiddingFinished}
+                  render={(state) => <BiddingCanvasExtras {...state} />} />
+              }
+            />
+          </Col>
+        )}
+      </Row>
     )
   }
 }
--- src/containers/Homepage/FinishedCanvases.js
+++ src/containers/Homepage/FinishedCanvases.js
@@ -5,15 +5,13 @@ import CanvasPreview from '../../components/Homepage/CanvasPreview'
 class FinishedCanvases extends React.PureComponent {
   render () {
     return (
-      <div className="overflow-hidden">
-        <Row gutter={100} type="flex">
-          {this.props.canvasIds.map((canvasId, index) =>
-            <Col xs={24} sm={12} md={6} key={index}>
-              <CanvasPreview canvasId={canvasId} />
-            </Col>
-          )}
-        </Row>
-      </div>
+      <Row gutter={100} type="flex">
+        {this.props.canvasIds.map((canvasId, index) =>
+          <Col xs={24} sm={12} md={6} key={index}>
+            <CanvasPreview canvasId={canvasId} />
+          </Col>
+        )}
+      </Row>
     )
   }
 }
