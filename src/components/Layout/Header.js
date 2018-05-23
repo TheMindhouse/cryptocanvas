708b76f0227aa008a8b06c2602befb5f0c139da2
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-05-23 09:24:09 +0200

    Testnet info in header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,40 @@
 // @flow
 import React from 'react'
-import { Popover, Row } from 'antd'
+import { Icon, Popover, Row } from 'antd'
 
 import './styles/Header.css'
 import { Link, NavLink } from 'react-router-dom'
 import withWeb3 from '../../hoc/withWeb3'
 import { URLHelper } from '../../helpers/URLhelper'
 import { HashLink } from 'react-router-hash-link'
+import { CONFIG } from '../../config'
+import { METAMASK_NETWORK_NAMES, METAMASK_NETWORKS } from '../../constants/metamask'
 
 type Props = {
   // from withWeb3
   account?: string,
 }
+
+const HeaderTestNet = () => (
+  <Popover
+    content={
+      <Row type="flex" align="middle" className="HeaderTestnet__Popup">
+        <Icon type="exclamation-circle" className="HeaderTestnet__WarningIcon" style={{ fontSize: 30 }}/>
+        <span>
+          This is only a TEST version of CryptoCanvas.<br />
+          Go to <a href="https://cryptocanvas.art">cryptocanvas.art</a> for the real one.
+        </span>
+      </Row>
+    }
+    title=""
+    placement="top"
+    trigger="hover"
+  >
+    <span className="HeaderTestnet__Info">
+      {METAMASK_NETWORK_NAMES[ CONFIG.ETHEREUM_NETWORK ]} <small><Icon type="question-circle-o" /></small>
+    </span>
+  </Popover>
+)
 
 const Header = (props: Props) => {
   return (
@@ -19,6 +42,7 @@ const Header = (props: Props) => {
       <div>
         <Link to="/" className="Header__title"><h1>CryptoCanvas</h1></Link>
         <h2 className="Header__subtitle">Distributed art on the blockchain</h2>
+        {CONFIG.ETHEREUM_NETWORK !== METAMASK_NETWORKS.main && <HeaderTestNet />}
       </div>
       <ul className="Header__menu">
         <li>
@@ -29,11 +53,11 @@ const Header = (props: Props) => {
           </NavLink>
         </li>
         {/*<li>*/}
-          {/*<NavLink to="/trade"*/}
-                   {/*className="Header__menu-link"*/}
-                   {/*activeClassName="Header__menu-link--active">*/}
-            {/*Marketplace*/}
-          {/*</NavLink>*/}
+        {/*<NavLink to="/trade"*/}
+        {/*className="Header__menu-link"*/}
+        {/*activeClassName="Header__menu-link--active">*/}
+        {/*Marketplace*/}
+        {/*</NavLink>*/}
         {/*</li>*/}
         <li>
           <NavLink to="/about"
@@ -57,17 +81,17 @@ const Header = (props: Props) => {
         {
           !props.account &&
           <li>
-              <Popover
-                content={<span>Log into MetaMask to manage your account.
+            <Popover
+              content={<span>Log into MetaMask to manage your account.
                   See <HashLink to={URLHelper.help.installingMetamask}>Installing MetaMask</HashLink></span>}
-                title=""
-                placement="bottom"
-                trigger="hover"
-              >
+              title=""
+              placement="bottom"
+              trigger="hover"
+            >
                 <span className="Header__menu-link  Header__menu-link--disabled">
                   <span className="hidden-mobile">My</span> Account
                 </span>
-              </Popover>
+            </Popover>
           </li>
         }
       </ul>
