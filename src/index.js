603b75149df53117baf923888223558f9d57ba16
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-05-26 18:43:16 +0200

    Unregister service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import 'antd/dist/antd.css'
 import './index.css'
 import App from './containers/App'
-import registerServiceWorker from './registerServiceWorker'
+import { unregister } from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+unregister()
