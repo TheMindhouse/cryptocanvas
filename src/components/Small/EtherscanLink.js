9bdb9d69ec98c7e36200113773331d188a10c394
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-05-29 16:49:13 +0200

    Choose a proper Etherscan url depending on the selected network

--- src/components/Small/EtherscanLink.js
+++ src/components/Small/EtherscanLink.js
@@ -1,17 +1,31 @@
 // @flow
 import * as React from 'react'
+import { CONFIG } from '../../config'
+import { METAMASK_NETWORKS } from '../../constants/metamask'
 
 type Props = {
   hash: string,
 }
 
+const getEtherscanUrl = () => {
+  switch (CONFIG.ETHEREUM_NETWORK) {
+    case METAMASK_NETWORKS.rinkeby:
+      return 'rinkeby.etherscan.io'
+    case METAMASK_NETWORKS.main:
+    default:
+      return 'etherscan.io'
+  }
+}
+
 class EtherscanLink extends React.PureComponent<Props> {
   static defaultProps = {}
-  
-  render() {
+
+  render () {
     const { hash } = this.props
+
+    const etherscanUrl = getEtherscanUrl()
     return (
-      <a href={`https://rinkeby.etherscan.io/tx/${hash}`} target="_blank"
+      <a href={`https://${etherscanUrl}/tx/${hash}`} target="_blank"
          className="font-monospace text-nowrap"
          rel="noopener noreferrer">{hash.substr(0, 8)}</a>
     )
