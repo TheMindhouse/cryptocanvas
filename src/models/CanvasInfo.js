f39414043bb008e83365ff2e41d4223296c48bbd
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-06-08 12:25:12 +0200

    Updated CanvasInfo model

--- src/models/CanvasInfo.js
+++ src/models/CanvasInfo.js
@@ -3,14 +3,19 @@ import { CanvasState } from './CanvasState'
 export class CanvasInfo {
   constructor (props) {
     this._id = parseInt(props[0], 10)
-    this._paintedPixels = parseInt(props[1], 10)
-    this._canvasState = new CanvasState(this.id, parseInt(props[2], 10))
-    this._initialBiddingFinishTime = props[3]
-    this._owner = props[4]
+    this._name = props[1]
+    this._paintedPixels = parseInt(props[2], 10)
+    this._canvasState = new CanvasState(this.id, parseInt(props[3], 10))
+    this._initialBiddingFinishTime = props[4]
+    this._owner = props[5]
   }
 
   get id () {
     return this._id
+  }
+
+  get name () {
+    return this._name
   }
 
   get paintedPixels () {
