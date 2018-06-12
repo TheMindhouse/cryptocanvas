a29b9a7b7429fcfe26b4f0fc7eba2da4bb3f4d8f
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-06-12 19:26:31 +0200

    Save displayed FB dialogs in session storage, show dialog in Help. Display Account Status on the left side

--- src/components/Layout/FbMessenger.js
+++ src/components/Layout/FbMessenger.js
@@ -8,7 +8,7 @@ const FbMessenger = () => {
          page_id="1882641792033739"
          theme_color="#0084ff"
          greeting_dialog_display="hide"
-         logged_in_greeting="Hi! I'll be here if you need any help!"
+         logged_in_greeting="Hi! I'm here if you need any help!"
          logged_out_greeting="Hi! I'm here if you need any help!" />
   )
 }
--- src/helpers/FbMessengerHelper.js
+++ src/helpers/FbMessengerHelper.js
@@ -6,32 +6,73 @@ declare var FB: {
   }
 }
 
+type MessengerData = {
+  GET_STARTED_DIALOG_SHOWN?: boolean,
+  PIXEL_PAINTED_DIALOG_SHOWN?: boolean,
+  HELP_DIALOG_SHOWN?: boolean,
+}
+
+const STORAGE_KEY = 'FB_MESSENGER_HELPER'
+
 const isSDKLoaded = () =>
   typeof FB === 'object' &&
   typeof FB.CustomerChat === 'object' &&
   typeof FB.CustomerChat.update === 'function' &&
   typeof FB.CustomerChat.showDialog === 'function'
 
+const getMessengerData = (): MessengerData => {
+  const messengerData = window.sessionStorage.getItem(STORAGE_KEY) || '{}'
+  return JSON.parse(messengerData)
+}
+
+const saveMessengerData = (messengerData: MessengerData) =>
+  window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(messengerData))
+
+
 export const FbMessengerHelper = {
   showGetStartedDialog: () => {
-    if (!isSDKLoaded()) {
+    const messengerData: MessengerData = getMessengerData()
+    if (!isSDKLoaded() || messengerData.GET_STARTED_DIALOG_SHOWN) {
       return
     }
+
     FB.CustomerChat.update({
-      logged_in_greeting: 'Hi! I\'ll be here if you need any help!',
-      logged_out_greeting: 'Hi! Log in to chat if you need any help!',
+      logged_in_greeting: 'Hi, we\'re here if you need any help!',
+      logged_out_greeting: 'Hi, we\'re here if you need any help!',
     })
     FB.CustomerChat.showDialog()
+
+    saveMessengerData({ ...messengerData, GET_STARTED_DIALOG_SHOWN: true })
   },
+
   showPixelPaintedDialog: () => {
-    if (!isSDKLoaded() || window.pixelPaintedDialogShown) {
+    const messengerData: MessengerData = getMessengerData()
+    if (!isSDKLoaded() || messengerData.PIXEL_PAINTED_DIALOG_SHOWN) {
       return
     }
+
     FB.CustomerChat.update({
-      logged_in_greeting: 'We have a Painter over here! How\'s your experience so far? :)',
-      logged_out_greeting: 'We have a Painter over here! How\'s your experience so far? :)',
+      logged_in_greeting: 'We got a Painter over here! How\'s your experience so far?',
+      logged_out_greeting: 'We got a Painter over here! How\'s your experience so far?',
     })
     FB.CustomerChat.showDialog()
-    window.pixelPaintedDialogShown = true
-  }
+
+    saveMessengerData({ ...messengerData, PIXEL_PAINTED_DIALOG_SHOWN: true })
+  },
+
+  showHelpDialog: () => {
+    const messengerData: MessengerData = getMessengerData()
+    if (!isSDKLoaded() || messengerData.HELP_DIALOG_SHOWN) {
+      return
+    }
+
+    FB.CustomerChat.update({
+      logged_in_greeting: 'Hi, how can we help you?',
+      logged_out_greeting: 'Hi, how can we help you?',
+    })
+    FB.CustomerChat.showDialog()
+
+    saveMessengerData({ ...messengerData, HELP_DIALOG_SHOWN: true })
+  },
+
 }
