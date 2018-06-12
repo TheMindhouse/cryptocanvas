47f8bfb68d7749a77886bf610c24acd021c9828a
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-06-12 21:09:23 +0200

    Fix intro page title

--- src/containers/Intro.js
+++ src/containers/Intro.js
@@ -5,23 +5,28 @@ import './styles/Intro.css'
 import BetaInfo from './Homepage/BetaInfo'
 import { CONFIG } from '../config'
 import { METAMASK_NETWORKS } from '../constants/metamask'
+import { setDocumentTitle } from '../helpers/utils'
 
-const Intro = () => {
-  return (
-    <div>
-      {
-        CONFIG.ETHEREUM_NETWORK !== METAMASK_NETWORKS.main &&
-        <BetaInfo />
-      }
-      <div className="container">
-        <div className="Intro__video">
-          <YoutubePlayer videoId="qRbZ_wcWzL0" />
+class Intro extends React.PureComponent<{}> {
+  componentDidMount () {
+    setDocumentTitle(null)
+  }
+
+  render () {
+    return (
+      <div>
+        {
+          CONFIG.ETHEREUM_NETWORK !== METAMASK_NETWORKS.main &&
+          <BetaInfo />
+        }
+        <div className="container">
+          <div className="Intro__video">
+            <YoutubePlayer videoId="qRbZ_wcWzL0" />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-Intro.defaultProps = {}
 
 export default Intro
