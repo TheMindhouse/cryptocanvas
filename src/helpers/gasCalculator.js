1680d0979148ea235332c7f9debda91ba84bb05b
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2018-06-14 13:32:10 +0200

    Updated ABI

--- src/helpers/gasCalculator.js
+++ src/helpers/gasCalculator.js
@@ -4,7 +4,8 @@ export const gasCalculator = {
   setPixels: (pixelsCount: number, hasFirstPixel: boolean):number => {
     const FIRST_PIXEL = 55000
     const REGULAR_PIXEL = 45000
-    let totalGas = pixelsCount * REGULAR_PIXEL
+    const EXTRA_OPERATIONS = 30000
+    let totalGas = pixelsCount * REGULAR_PIXEL + EXTRA_OPERATIONS
     if (hasFirstPixel) totalGas += FIRST_PIXEL
     return totalGas
   },
