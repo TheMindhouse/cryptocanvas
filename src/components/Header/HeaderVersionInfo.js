9c6f21896e24f6e9b6e34457af2e191f3d3e644f
Chris Gaszynski <k.gaszynski@gmail.com>
2021-03-14 15:29:32 +0200

    Change header version info

--- src/components/Header/HeaderVersionInfo.js
+++ src/components/Header/HeaderVersionInfo.js
@@ -6,29 +6,27 @@ import './styles/HeaderVersionInfo.css'
 
 const MainVersion = () => (
   <div>
-    <span className="HeaderVersionInfo__Version HeaderVersionInfo__Version--main">Main Net</span>
-    <span className="HeaderVersionInfo__Caption">Play around for free on <a href="https://rinkeby.cryptocanvas.art">rinkeby.cryptocanvas.art</a></span>
+    <a href="https://cryptocanvas.art">
+      <span className={`HeaderVersionInfo__Version HeaderVersionInfo__Version--main 
+      ${CONFIG.ETHEREUM_NETWORK !== METAMASK_NETWORKS.main ? 'HeaderVersionInfo__Version--inactive' : ''}`}>Main Network</span>
+    </a>
   </div>
 )
 
 const TestnetVersion = () => (
   <div>
-    <span className="HeaderVersionInfo__Version HeaderVersionInfo__Version--testnet">Rinkeby</span>
-    <span className="HeaderVersionInfo__Caption">Check out Main Net version at <a href="https://cryptocanvas.art">cryptocanvas.art</a></span>
+    <a href="https://rinkeby.cryptocanvas.art">
+      <span className={`HeaderVersionInfo__Version HeaderVersionInfo__Version--testnet 
+      ${CONFIG.ETHEREUM_NETWORK !== METAMASK_NETWORKS.rinkeby ? 'HeaderVersionInfo__Version--inactive' : ''}`}>Rinkeby Testnet</span>
+    </a>
   </div>
 )
 
 const HeaderVersionInfo = () => {
   return (
     <div className="HeaderVersionInfo">
-      {
-        CONFIG.ETHEREUM_NETWORK === METAMASK_NETWORKS.main &&
         <MainVersion />
-      }
-      {
-        CONFIG.ETHEREUM_NETWORK === METAMASK_NETWORKS.rinkeby &&
         <TestnetVersion />
-      }
     </div>
   )
 }
